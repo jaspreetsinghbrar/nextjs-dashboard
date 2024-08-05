1a408fe703cf936e662d911782f80b1b29277369
jaspreetsinghbrar <jas.brar@ymail.com.com>
2024-08-05 12:55:04 +0200

    f

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -105,17 +105,4 @@ export async function GET() {
   return Response.json({
    
   });
-   try {
-     await client.sql`BEGIN`;
-     await seedUsers();
-     await seedCustomers();
-    await seedInvoices();
-     await seedRevenue();
-    await client.sql`COMMIT`;
-
-     return Response.json({ message: 'Database seeded successfully' });
-   } catch (error) {
-     await client.sql`ROLLBACK`;
-    return Response.json({ error }, { status: 500 });
-   }
 }
